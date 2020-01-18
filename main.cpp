74627b5e6b5821b3bd946c1f005a14a5f3968985
hyzboy <hyzboy@gmail.com>
2020-01-18 18:36:33 +0800

    update ShaderMaker

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 ﻿#include"glsl2spv.h"
+#include<hgl/type/StdString.h>
 #include<hgl/type/StringList.h>
 #include<iostream>
 
@@ -50,7 +51,33 @@ UTF8String MakeValueName(const ShaderDataType type,const uint count)
         return type_name[uint(type)];
 }
 
+enum class ShaderSegment
+{
+    None=0,
 
+    Attribute,
+    Output,
+    AttributeToGBuffer,
+    GBufferToAttribute
+
+
+};
+
+inline bool IsSegmentFlag(const UTF8String &str)
+{
+    return(str.GetBeginChar()   =='['
+         &&str.GetEndChar()     ==']');
+}
+
+inline const ShaderSegment &GetShaderSegment(const UTF8String &str)
+{
+    if(str.CaseComp("[attribute]"           )==0)return ShaderSegment::Attribute;
+    if(str.CaseComp("[output]"              )==0)return ShaderSegment::Output;
+    if(str.CaseComp("[attribute_to_gbuffer]")==0)return ShaderSegment::AttributeToGBuffer;
+    if(str.CaseComp("[gbuffer_to_attribute]")==0)return ShaderSegment::GBufferToAttribute;
+
+    return ShaderSegment::None;
+}
 
 #if HGL_OS == HGL_OS_Windows
 int wmain(int argc,wchar_t **argv)
@@ -70,9 +97,21 @@ int main(int argc,char **argv)
 
     UTF8StringList gbfile;
 
+    ShaderSegment cur_segment=ShaderSegment::None;
+
     const int lines=LoadStringListFromTextFile(gbfile,argv[1]);
 
     std::cout<<"file total lines="<<lines<<std::endl;
+
+    for(int i=0;i<lines;i++)
+    {
+        const UTF8String &str=gbfile.GetString(i);
+
+        if(IsSegmentFlag(str))
+            cur_segment=GetShaderSegment(str);
+
+        
+    }
 
     return(0);
 }
