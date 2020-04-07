1638b8ccc74f957f61c4abb2549cd016793fe2f9
hyzboy <hyzboy@gmail.com>
2020-04-07 18:34:00 +0800

    拆分代码

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
-﻿#include"glsl2spv.h"
+﻿#include"ShaderDataType.h"
+#include"glsl2spv.h"
+
 #include<hgl/type/StdString.h>
 #include<hgl/type/StringList.h>
 #include<hgl/io/FileOutputStream.h>
@@ -12,107 +14,6 @@ using namespace hgl;
 
 bool use_opengl_es=false;
 bool use_subpass=false;
-
-constexpr size_t MAX_SHADER_VALUE_NAME_LENGTH=32;
-
-enum class ShaderDataType
-{
-    Bool=0,
-    Float,
-    Int,
-    Uint,
-    Double,
-
-    BEGIN_RANGE =Bool,
-    END_RANGE   =Double,
-    RANGE_SIZE  =(END_RANGE-BEGIN_RANGE+1)
-};//
-
-using ShaderDataFormat=uint32;
-
-inline ShaderDataFormat MakeShaderDataFormat(const ShaderDataType type,const uint count)
-{
-    if(type<ShaderDataType::BEGIN_RANGE
-     ||type>ShaderDataType::END_RANGE)return 0;
-
-    if(count<1||count>4)return 0;
-
-    return((uint(type)<<2)|(count-1));
-}
-
-UTF8String MakeValueName(const ShaderDataType type,const uint count)
-{
-    constexpr char type_name[][8]=
-    {
-        "bool",
-        "float",
-        "int",
-        "uint",
-        "double"
-    };
-
-    constexpr char type_vec_name[][8]=
-    {
-        "bvec",
-        "vec",
-        "ivec",
-        "uvec",
-        "dvec"
-    };
-
-    if(type<ShaderDataType::BEGIN_RANGE
-     ||type>ShaderDataType::END_RANGE)
-        return("[ERROR ShaderDataType]");
-
-    if(count<1||count>4)
-        return("[ERROR Type component count]");
-
-    if(count==1)
-        return type_name[uint(type)];
-
-    return UTF8String(type_vec_name[uint(type)])+UTF8String(count);
-}
-
-UTF8String MakeValueName(const ShaderDataFormat sdf)
-{
-    uint type,count;
-
-    type=sdf>>2;
-    count=sdf&3;
-
-    return MakeValueName((ShaderDataType)type,count+1);
-}
-
-
-const ShaderDataFormat ParseShaderType(const UTF8String &str)
-{
-    const char first_char=str.GetBeginChar();
-    const char end_char=str.GetEndChar();
-
-    if(first_char=='f')return MakeShaderDataFormat(ShaderDataType::Float,1);
-
-    uint count=0;
-
-    if(end_char>='2'&&end_char<='4')        //vec
-        count=end_char-'0';
-    else 
-        count=1;
-
-    if(first_char=='v')return MakeShaderDataFormat(ShaderDataType::Float,   count);else
-    if(first_char=='b')return MakeShaderDataFormat(ShaderDataType::Bool,    count);else
-    if(first_char=='i')return MakeShaderDataFormat(ShaderDataType::Int,     count);else
-    if(first_char=='u')return MakeShaderDataFormat(ShaderDataType::Uint,    count);else
-    if(first_char=='d')return MakeShaderDataFormat(ShaderDataType::Double,  count);else
-        return 0;   
-}
-    
-struct ShaderAttribute
-{
-    ShaderDataFormat format;
-    char name[MAX_SHADER_VALUE_NAME_LENGTH+1];
-};
-
-using ShaderAttributeList=List<ShaderAttribute>;
 
 ShaderAttributeList attr_list;
 ShaderAttributeList gbuffer_list;
