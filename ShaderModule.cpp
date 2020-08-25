1595a1bbd89d92fd204bc375a4291f11d53af97e
hyzboy <hyzboy@gmail.com>
2020-08-25 21:31:04 +0800

    add "shader stage" parse

--- ShaderModule.cpp
+++ ShaderModule.cpp
@@ -27,21 +27,21 @@ namespace shader
 
         class CPPElementCreater:public ElementCreater
         {
-            UBO *ubo;
+            SubModule *sm;
 
         public:
 
-            CPPElementCreater():ElementCreater("cpp"){ubo=nullptr;}
+            CPPElementCreater():ElementCreater("cpp"){sm=nullptr;}
             virtual ~CPPElementCreater()=default;
 
-            void SetUBO(UBO *su){ubo=su;}
-            void End() override {ubo=nullptr;}
+            void Set(SubModule *_sm){sm=_sm;}
+            void End() override {sm=nullptr;}
 
             void CharData(const char *str,const int str_length) override
             {
-                if(!ubo)return;
+                if(!sm)return;
 
-                ubo->cpp_files.Add(UTF8String((u8char *)str,str_length));
+                sm->cpp_files.Add(UTF8String((u8char *)str,str_length));
             }
         };//class CPPElementCreater:public ElementCreater
         
@@ -113,7 +113,7 @@ namespace shader
             {
                 ubo=new UBO;
 
-                cpp_ec.SetUBO(ubo);
+                cpp_ec.Set(ubo);
                 code_ec.SetUBO(ubo);
 
                 return(true);
@@ -121,6 +121,8 @@ namespace shader
 
             void Attr(const char *flag,const char *info) override
             {
+                if(!ubo)return;
+
                 if(strcmp(flag,"name")==0)ubo->name=info;else
                 if(strcmp(flag,"value")==0)ubo->value_name=info;                
             }
@@ -135,46 +137,104 @@ namespace shader
             }
         };//class UBOElementCreater:public ElementCreater
 
-        class ShaderRootElementCreater:public ElementCreater
+        class VertexAttrribElementCreater:public ElementCreater
+        {
+            Module *shader_module;
+
+            graph::VertexAttribType vat;
+            UTF8String name;
+            UTF8String comment;
+
+        public:
+
+            VertexAttrribElementCreater(Module *sm):ElementCreater("vertex_attrib"){shader_module=sm;}
+            virtual ~VertexAttrribElementCreater()=default;
+            
+            void Attr(const char *flag,const char *info) override
+            {
+                if(!shader_module)return;
+
+                if(strcmp(flag,"type")==0)graph::ParseVertexAttribType(&vat,info);else
+                if(strcmp(flag,"name")==0)name=(u8char *)info;else
+                if(strcmp(flag,"comment")==0)comment=(u8char *)info;
+            }
+
+            void End() override
+            {
+                if(!shader_module)return;
+
+                ShaderStage *ss=new ShaderStage;
+
+                ss->type=vat;
+                ss->name=name;
+                ss->comment=comment;
+
+                shader_module->shader_stage.Add(ss);
+
+                const char *type_string=graph::GetVertexAttribName(&(ss->type));
+
+                std::cout<<"shader stage: "<<type_string<<" "<<name.c_str()<<";\t//"<<comment.c_str()<<std::endl;
+            }
+        };//class VertexAttrribElementCreater:public ElementCreater
+        
+        class RootElementCreater:public ElementCreater
         {        
-            AuthorElementCreater author_ec;
-            TimeElementCreater time_ec;
+            VertexAttrribElementCreater *va_ec;
             UBOElementCreater *ubo_ec;
 
             Module *shader_module;
 
         public:
 
-            ShaderRootElementCreater(Module *sm):ElementCreater("root")
+            RootElementCreater(Module *sm):ElementCreater("root")
             {
                 shader_module=sm;
 
+                va_ec=new VertexAttrribElementCreater(sm);
                 ubo_ec=new UBOElementCreater(sm);
 
-                //Registry("author",&author_ec);
-                //Registry("time",&time_ec);
+                Registry("vertex_attrib",va_ec);
                 Registry("ubo",ubo_ec);
             }
 
-            virtual ~ShaderRootElementCreater()
+            virtual ~RootElementCreater()
             {
                 SAFE_CLEAR(ubo_ec);
+                SAFE_CLEAR(va_ec);
+            }
+        };//class RootElementCreater:public ElementCreater
+
+        class ShaderElementCreater:public ElementCreater
+        {
+            Module *shader_module;
+
+            RootElementCreater *root_ec;
+
+        public:
+
+            ShaderElementCreater(Module *sm):ElementCreater("shader")
+            {
+                shader_module=sm;
+                root_ec=new RootElementCreater(sm);
+
+                Registry("root",root_ec);
             }
 
-            void End() override
+            ~ShaderElementCreater()
             {
+                SAFE_CLEAR(root_ec);
             }
-        };//class ShaderRootElementCreater:public ElementCreater
+        };//class ShaderElementCreater:public ElementCreater
     }//namespace
 
-    Module *LoadXMLShader(const OSString &filename)
+    Module *LoadXMLShader(const XMLShaderModuleType &type_hint,const OSString &filename)
     {
         Module *sm=new Module;
 
         sm->filename=filename;
 
-        ShaderRootElementCreater root_ec(sm);
-        ElementParseCreater epc(&root_ec);
+        ShaderElementCreater shader_ec(sm);
+        ElementParseCreater epc(&shader_ec);
         XMLParse xp(&epc);
 
         xp.Start();
--- ShaderModule.h
+++ ShaderModule.h
@@ -3,41 +3,79 @@
 
 #include<hgl/type/StringList.h>
 #include<hgl/type/DateTime.h>
+#include<hgl/graph/VertexAttrib.h>
 namespace shader
 {
     using namespace hgl;
+
+    /**
+     * XML着色模块类型
+     */
+    enum class XMLShaderModuleType
+    {
+        Common=0,           ///<一般公共模块
+
+        PushConstant,
+
+        VertexAttrib,       ///<顶点属性
+        FragmentOutput,     ///<片段着色器输出流        
+
+        Vertex,             ///<顶点着色器
+        TessControl,
+        TessEvaluation,
+        Geometry,
+        Fragment,
+        Compute,
+
+        Raygen,
+        AnyHit,
+        ClosestHit,
+        Miss,
+        Intersection,
+        Callable,
+        Task,
+        Mesh,
+
+        ENUM_CLASS_RANGE(Common,Mesh)
+    };//enum class XMLShaderModuleType
 
     struct PushConstant
     {
         UTF8StringList codes;
     };//struct PushConstant
 
-    struct UBO
+    struct SubModule
+    {
+        UTF8StringList cpp_files;       ///<c++代码相关文件
+    };
+
+    struct UBO:public SubModule
     {
         UTF8String name;                ///<UBO结构名
         UTF8String value_name;          ///<UBO名变量(会增加vert/frag/geo等前缀)
 
-        UTF8StringList cpp_files;       ///<c++代码相关文件
         UTF8StringList codes;           ///<代码
     };//struct UBO
+
+    struct ShaderStage:public SubModule
+    {
+        graph::VertexAttribType type;
+        UTF8String name;
+        UTF8String comment;
+    };
 
     /**
      * 模组
      */
     struct Module
     {
-        UTF8String  module_name;        ///<模组名称
-        OSString    filename;           ///<模组源文件
+        UTF8String  module_name;                ///<模组名称
+        OSString    filename;                   ///<模组源文件
 
-        //UTF8String  author;             ///<作者名称
-        //UTF8String  email;              ///<作者email地址
-
-        //DateTime    time_first;         ///<初次创作时间
-        //DateTime    time_last;          ///<最后更新时间
-
-        ObjectList<UBO> ubo_list;
+        ObjectList<ShaderStage> shader_stage;
+        ObjectList<UBO>         ubo_list;
     };//struct Module
 
-    Module *LoadXMLShader(const OSString &filename);
+    Module *LoadXMLShader(const XMLShaderModuleType &type_hint,const OSString &filename);
 }//namespace shader
 #endif//SHADER_COMPILER_MODULE_INCLUDE
