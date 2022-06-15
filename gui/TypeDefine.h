dd402c2b8a8214df7cc1bff41016d12f2ba4fed4
HuYingzhuo(hugo/hyzboy) <hyzboy@outlook.com>
2022-06-15 21:52:22 +0800

    added all shader type

--- gui/TypeDefine.h
+++ gui/TypeDefine.h
@@ -11,17 +11,44 @@ enum class MaterialFileType
 
     Material,
     Vert,
+    Tesc,
+    Tese,
     Geom,
     Frag,
+    Comp,
+    Mesh,
+    Task,
+    Rgen,
+    Rint,
+    Rahit,
+    Rchit,
+    Rmiss,
+    Rcall,
 
-    ENUM_CLASS_RANGE(None,Frag)
+    END,
+
+    ENUM_CLASS_RANGE(None,END)
 };
 
-constexpr const char *MaterialFileTypeName[16]=
+constexpr const char *MaterialFileTypeName[(size_t)MaterialFileType::RANGE_SIZE]=
 {
     "None",
+    
     "Material",
     "Vert",
+    "Tesc",
+    "Tese",
     "Geom",
     "Frag",
+    "Comp",
+    "Mesh",
+    "Task",
+    "Rgen",
+    "Rint",
+    "Rahit",
+    "Rchit",
+    "Rmiss",
+    "Rcall",
+    
+    "END"
 };
