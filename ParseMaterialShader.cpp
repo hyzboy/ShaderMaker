682b08bf937884beffbe172dee204fb18778a8c5
HuYingzhuo(hugo/hyzboy) <hyzboy@outlook.com>
2022-08-03 19:37:23 +0800

    added few codes in ParseMaterialShader.cpp

--- ParseMaterialShader.cpp
+++ ParseMaterialShader.cpp
@@ -4,6 +4,18 @@
 
 namespace shader_lib
 {
+    class MaterialShader
+    {
+        ShaderType shader_type;
+
+        OSString origin_filename;
+        UTF8String short_name;
+
+        UTF8StringList includes;
+        UTF8StringList in,out;
+        GeometryAttribute geom;
+    };
+
     bool LoadMaterialShader(const ShaderType &type,const OSString &filename,InfoOutput *info_output)
     {
         UTF8StringList sl;
@@ -13,6 +25,9 @@ namespace shader_lib
         if(line_count<=0)
             return(false);
 
-
+        for(int i=0;i<line_count;i++)
+        {
+            
+        }
     }
 }//namespace shader_lib
